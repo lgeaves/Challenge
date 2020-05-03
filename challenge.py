50011408e141a386b2d87489d6656bee6f8a7911
lgeaves <64633562+lgeaves@users.noreply.github.com>
2020-05-03 12:22:54 +0100

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,22 @@
+import xlrd
+import csv
+import urllib.request
+import datetime
+
+local_filename, _ = urllib.request.urlretrieve('https://www.eia.gov/dnav/ng/hist_xls/RNGWHHDd.xls')
+
+wb = xlrd.open_workbook(local_filename)
+sh = wb.sheet_by_name('Data 1')
+gas_prices = open('gas_prices.csv', 'w')
+wr = csv.writer(gas_prices, quoting=csv.QUOTE_ALL)
+wr.writerow(sh.row_values(2))
+	
+for rownum in range(3, sh.nrows):
+	row = sh.row_values(rownum)
+	excel_date = row[0]
+	python_date = datetime.datetime(*xlrd.xldate_as_tuple(excel_date, 0))
+	row[0] = python_date.strftime("%Y-%m-%d")
+	wr.writerow(row)
+
+gas_prices.close()
+	
